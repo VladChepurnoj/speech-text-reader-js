b3dd4a8653ca2203225755f06c07861c9485d5ce
unknown <chepurnojv@gmail.com>
2022-01-31 19:21:23 +0200

    Get Speech Voices

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const voicesSelect = document.getElementById("voices");
 const textarea = document.getElementById("text");
 const readBtn = document.getElementById("read");
 const toggleBtn = document.getElementById("toggle");
-const clsoeBtn = document.getElementById("close");
+const closeBtn = document.getElementById("close");
 
 const data = [
   {
@@ -72,3 +72,33 @@ function createBox(item) {
 
   main.appendChild(box);
 }
+
+//store voices
+let voices = [];
+
+function getVoices() {
+  voices = speechSynthesis.getVoices();
+
+  voices.forEach((voice) => {
+    const option = document.createElement("option");
+    option.value = voice.name;
+    option.innerText = `${voice.name} ${voice.lang}`;
+
+    voicesSelect.appendChild(option);
+  });
+}
+
+//voices changed
+speechSynthesis.addEventListener("voiceschanged", getVoices);
+
+//toggle textbox
+toggleBtn.addEventListener("click", () =>
+  document.getElementById("text-box").classList.toggle("show")
+);
+
+//close textbox
+closeBtn.addEventListener("click", () =>
+  document.getElementById("text-box").classList.remove("show")
+);
+
+getVoices();
